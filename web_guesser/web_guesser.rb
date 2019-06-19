c4e333689ab155371de9e2032470f2d0e8555cef
Nick Schoch <nschoch@gmail.com>
2019-06-19 07:03:17 -0700

    add guess strike out

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -1,28 +1,42 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-RANDOM_NUMBER = rand(100)
+@@random_number = rand(100)
+@@guess_count = 5
 
 get '/' do
   guess = params['guess']
-  message = check_guess(guess)
-  
-  erb :index, :locals => {:random_number => RANDOM_NUMBER, :message => message}
+  if guess != ''
+    message = check_guess(guess)
+    @@guess_count -= 1
+    message += "<br>#{@@guess_count} guesses remaining."
+  else
+    message = 'Make a guess!'
+  end
+
+  if @@random_number == guess.to_i || @@guess_count == 0
+    @@random_number = rand(100)
+    @@guess_count = 5
+    message += '<br>New random number generated with 5 tries to guess'
+  end
+
+  locals = {:random_number => @@random_number, :message => message, :guess_count => @@guess_count}
+
+  erb :index, :locals => locals
+
 end
 
 def check_guess(guess)
   guess = guess.to_i
-  if guess == 0
-    ''
-  elsif guess > RANDOM_NUMBER + 5
+  if guess > @@random_number + 5
     "Way too high!"
-  elsif guess > RANDOM_NUMBER
+  elsif guess > @@random_number
     "Too high"
-  elsif guess == RANDOM_NUMBER
-    "You got it!<br>The SECRET NUMBER is #{RANDOM_NUMBER}<br>"
-  elsif guess < RANDOM_NUMBER - 5
+  elsif guess == @@random_number
+    "You got it!<br>The SECRET NUMBER is #{@@random_number}<br>"
+  elsif guess < @@random_number - 5
     "Way too low!"
-  elsif guess < RANDOM_NUMBER
+  elsif guess < @@random_number
     "Too low"
   else
     "Something went wrong"
